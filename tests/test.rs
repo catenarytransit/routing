b906fd001f6c48bc198b954cfc9d7073238ed24b
mysteriouspresence <chelseawen03@gmail.com>
2025-01-25 15:03:42 -0800

    oh yeah this works

--- tests/test.rs
+++ tests/test.rs
@@ -7,7 +7,6 @@ mod tests {
 
     #[test]
     fn test() {
-        let now = Instant::now();
         let savepath = "results.json";
 
         println!("generating transit network graph");
@@ -18,42 +17,6 @@ mod tests {
         let (transit_graph, connections) = TimeExpandedGraph::new(gtfs, "Wednesday".to_string(), 0);
         let mut router = TransitDijkstra::new(&transit_graph);
 
-        //connection tests, these should be valid transfers --> success!
-
-        //https://maps.app.goo.gl/eXf4S5edPM8vgvVt9
-        /*println!("time for transit {:?}", now.elapsed());
-        let start_station = *transit_graph.station_map.get("9079").unwrap(); //Blue Hills Ave @ Home Goods
-        let end_station = *transit_graph.station_map.get("1682").unwrap(); //Bloomfield Ave @ Advo
-
-        let x = direct_connection_query(&connections, start_station, end_station, 25680); //7:08 AM
-        let y = direct_connection_query(&connections, 9079, 1682, 25680); //7:08 AM
-        println!("dc query {:?} and {:?}", x, y);*/
-
-        //https://maps.app.goo.gl/szffQJAEALqSeHNF7
-        /*let source_id = NodeId {
-            //Downtown New Britain Station @ Columbus Blvd Bay
-            node_type: NodeType::Arrival,
-            station_id: 13381,
-            time: Some(19200), //5:20 AM, 10 second transfer buffer
-            trip_id: 1758411,
-        };
-        let target_id = NodeId {
-            //Hart St @ Camp St
-            node_type: NodeType::Arrival,
-            station_id: 9738,
-            time: Some(19515),
-            trip_id: 1758411,
-        };
-        let now = Instant::now();
-        let d = router.time_expanded_dijkstra(vec![source_id], None);
-        println!("time for dijkstra {:?}", now.elapsed());
-
-        let r = d.get(&target_id).unwrap().clone().get_path();
-        println!("routing {:#?} and visted count {}", r.1, d.len());*/
-
-        //let mut test_node: NodeId = serde_json::from_str("\"Arrival,9079,19440,1752373\"").unwrap();
-        //println!("test node {:?}", test_node)
-
         //full routing test
         //see following link, anything but first option (which includes walking between stations, hasnt been implemented yet)
         //https://www.google.com/maps/dir/Bloomfield,+Connecticut+06002/77+Forest+St,+Hartford,+CT+06105/@41.823207,-72.7745391,34082m/data=!3m1!1e3!4m20!4m19!1m5!1m1!1s0x89e7001af40714d7:0xc4be608b22d7e4a8!2m2!1d-72.7197095!2d41.8683576!1m5!1m1!1s0x89e653502e880197:0xc1f0096f7d179457!2m2!1d-72.7005256!2d41.7671825!2m4!4e3!6e0!7e2!8j1727241000!3e3!5i1
@@ -61,7 +24,7 @@ mod tests {
         let preset_distance = 250.0;
 
         //pepperidge farm to harriet beecher stowe center
-        /*let (source, target) = make_points_from_coords(
+        let (source, target) = make_points_from_coords(
             41.86829675142084,
             -72.71973332600558,
             41.76726348091365,
@@ -78,15 +41,15 @@ mod tests {
             preset_distance,
         );
 
-        let mut output = File::create(savepath).unwrap();
+        let output = File::create(savepath).unwrap();
         println!("query graph constructed in {:?}", now.elapsed());
-        serde_json::to_writer(output, &graph).unwrap();*/
+        serde_json::to_writer(output, &graph).unwrap();
 
         //part 2
 
         let file = File::open(savepath).ok().unwrap();
         let reader = BufReader::new(file);
-        let mut graph: QueryGraph = serde_json::from_reader(reader).unwrap();
+        let graph: QueryGraph = serde_json::from_reader(reader).unwrap();
 
         //road network, for footpaths
         /*let now = Instant::now();
@@ -161,20 +124,5 @@ mod tests {
         pareto_recompute(&mut set, (7, 2));
         println!("{:?}", set);
         */
-
-        //early stage Direct-Connection Query Test
-        /*let connections = DirectConnections {
-            route_tables: HashMap::from([("L17".to_string(), LineConnectionTable {
-                times_from_start: HashMap::from([(154,0), (97,420), (987, 720), (111,1260)]),
-                start_times: Vec::from([29700, 33300, 36900, 40800, 44400])
-            })]),
-            lines_per_station: HashMap::from([(97, Vec::from([
-                    ("L8".to_string(), 4), ("L17".to_string(), 2),("L34".to_string(), 5), ("L87".to_string(), 17)])),
-                (111, Vec::from([
-                    ("L9".to_string(), 1), ("L13".to_string(), 5),("L17".to_string(), 4), ("L55".to_string(), 16)
-                ]))])
-        };
-        let query = direct_connection_query(&   connections, 97, 111, 37200);
-        println!("aa query results: {:?}", query);*/
     }
 }
